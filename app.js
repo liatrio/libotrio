903626494c1d05be59ccf7c75c180cd67015530d
Calvin Wai <cwai@Calvins-MacBook-Pro.local>
2021-06-30 10:37:27 -0700

    that was almost painful

---
+++ app.js
@@ -0,0 +1,16 @@
+const { App } = require('@slack/bolt');
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.BOT_USER_OAUTH_ACCESS_TOKEN,
+  signingSecret: process.env.SIGNING_SECRET,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
