ce6f51a55955421ed97fc116c6b427889f1b65f4
Chris Schreiber <chriss@liatrio.com>
2021-09-09 16:42:54 -0700

    feat: add helm chart to deploy libotrio (#4)
    * feat: add heath check endpoint
    
    * feat: add helm chart
    
    * fix: change repo name
    
    * feat: add helm upgrade step to deploy pipeline

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
 const { App } = require('@slack/bolt');
+const express = require('express');
+
+const webserver = express();
+
+webserver.get("/healthz", async (req, res) => {
+  const status_checks = {};
+
+  // Check Slack API
+  try {
+    const slack_api_status = await app.client.api.test();
+    if (slack_api_status.ok) {
+      status_checks.slack_api = "OK";
+    }
+  } catch (e) {
+    status_checks.slack_api = e.message;
+  }
+
+  // Check Slack Auth
+  try {
+    const slack_auth_status = await app.client.auth.test();
+    if (slack_auth_status.ok) {
+      status_checks.slack_auth = "OK";
+    }
+  } catch (e) {
+    status_checks.slack_auth = e.message;
+  }
+
+  status_checks.slack_websocket_connection = app.receiver.client.badConnection
+    ? "Connection Failed"
+    : "OK";
+
+  for (const i in status_checks) {
+    if (status_checks[i] !== "OK") {
+      res.status(500).send(status_checks);
+      return;
+    }
+  }
+  res.send(status_checks);
+})
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
-  token: process.env.BOT_USER_OAUTH_ACCESS_TOKEN,
-  signingSecret: process.env.SIGNING_SECRET,
+  token: process.env.SLACK_BOT_USER_OAUTH_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
   appToken: process.env.SLACK_APP_TOKEN,
   socketMode: true
 });
@@ -17,7 +56,8 @@ require("fs")
 
 (async () => {
   // Start your app
-  await app.start(process.env.PORT || 3000);
+  await app.start();
+  webserver.listen(process.env.PORT || 3000);
 
   console.log('⚡️ Bolt app is running!');
 })();
