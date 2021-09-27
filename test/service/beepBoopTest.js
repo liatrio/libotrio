1400b6a7ab973576e3aa310f974820b4370e432d
Austin White <austinw@liatrio.com>
2021-09-27 14:59:53 -0700

    chore: run lint-fix

--- test/service/beepBoopTest.js
+++ test/service/beepBoopTest.js
@@ -2,11 +2,11 @@ const expect = require("chai").expect;
 const beepBoop = require("../../service/beepBoopServ").respond;
 
 describe("service/beepBoop", () => {
-    describe("beepBoopServ", () => {
-        it("should count the number of boops since last restart", () => {
-            expect(beepBoop()).to.equal(`BOOP #1 since last restart`);
-            expect(beepBoop()).to.equal(`BOOP #2 since last restart`);
-            expect(beepBoop()).to.equal(`BOOP #3 since last restart`);
-        });
+  describe("beepBoopServ", () => {
+    it("should count the number of boops since last restart", () => {
+      expect(beepBoop()).to.equal(`BOOP #1 since last restart`);
+      expect(beepBoop()).to.equal(`BOOP #2 since last restart`);
+      expect(beepBoop()).to.equal(`BOOP #3 since last restart`);
     });
+  });
 });
