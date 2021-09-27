ff06964c7e836a91ec318188e7973b55429a6d21
chodges7 <chodges7@mail.csuchico.edu>
2021-09-27 14:40:56 -0700

    feat: add connection pools

--- repositories/pool.js
+++ repositories/pool.js
@@ -1,3 +1,20 @@
-// create pool at startup
+const mysql = require("mysql2/promise");
 
 // grab connection from the pool
+// create pool at startup
+
+module.exports = {
+  setupPool: () => {
+    const pool = mysql.createPool({
+      host: process.env.MYSQL_HOSTNAME,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      waitForConnections: true,
+      connectionLimit: 5,
+      queueLimit: 0,
+    });
+  },
+
+  getConnection: () => {},
+};
