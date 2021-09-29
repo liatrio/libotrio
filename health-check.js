eaa4937ccf989d1e5e497a8a85d7e696ee250791
Chris Schreiber <chriss@liatrio.com>
2021-09-29 16:57:23 -0700

    feat: add docker compose health check for bot

---
+++ health-check.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+(async () => {
+    try {
+        const response = await axios.get('http://localhost:3000/healthz');
+        console.log("OK", response.data);
+        process.exit(0);
+    } catch (err) {
+        console.log("ERROR", err);
+        process.exit(1)
+    }
+})();
