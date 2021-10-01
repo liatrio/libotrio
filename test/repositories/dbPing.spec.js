622809bdefecf3d47cf2e97de0d1be039d7050bf
austinjw <87999593+austinjw@users.noreply.github.com>
2021-10-01 07:29:34 -0700

    feat: add dbPing and dependencies (#23)
    * feat: add dbPing test and dependancies
    
    * chore: npm run lint-fix
    
    * chore: update package-lock
    
    * chore: refactored dbPing test

---
+++ test/repositories/dbPing.spec.js
@@ -0,0 +1,69 @@
+const { expect } = require("chai");
+const Chance = require("chance");
+const sinon = require("sinon");
+const { getPings } = require("../../repositories/pings");
+const databaseOps = require("../../repositories/databaseOps");
+
+describe("repositories/pings", () => {
+  let chance;
+  let sandBox;
+  let returnValue;
+  let queryStub;
+
+  beforeEach(() => {
+    chance = new Chance();
+    returnValue = chance.integer();
+    sandBox = sinon.createSandbox();
+    queryStub = sandBox
+      .stub()
+      .onSecondCall()
+      .resolves([
+        [
+          {
+            pings: returnValue,
+          },
+        ],
+      ]);
+    sandBox.stub(databaseOps, "getPool").returns({
+      getConnection: () => {
+        return {
+          query: queryStub,
+          release: sandBox.stub().resolves(),
+        };
+      },
+    });
+  });
+
+  afterEach(() => {
+    sandBox.restore();
+  });
+
+  describe("getPings", () => {
+    it("should count the number of pings since database initalization", async () => {
+      const pings = await getPings("user");
+      expect(pings).to.equal(returnValue);
+    });
+
+    it("should increment the ping value for the user in the database", async () => {
+      let aUser = chance.word();
+      const pings = await getPings(aUser);
+      expect(queryStub).to.have.callCount(2);
+      expect(queryStub).to.have.been.calledWith({
+        sql: "INSERT INTO profiles(name, pings) VALUES(?, 1) ON DUPLICATE KEY UPDATE pings = pings + 1",
+        timeout: 30000, // 30s
+        values: [aUser],
+      });
+    });
+
+    it("should retreive the ping value for the user in the database", async () => {
+      let aUser = chance.word();
+      const pings = await getPings(aUser);
+      expect(queryStub).to.have.callCount(2);
+      expect(queryStub).to.have.been.calledWith({
+        sql: "SELECT pings FROM profiles WHERE name = ?",
+        timeout: 30000, // 30s
+        values: [aUser],
+      });
+    });
+  });
+});
