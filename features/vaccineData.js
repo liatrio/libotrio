a8a466fd6fa483dfc6a58b925cc5b57c1e7b905a
Micah Perez II <micahperez@Micahs-MacBook-Pro.local>
2021-10-04 10:43:46 -0700

    chore: run lint fix on vaccine files

--- features/vaccineData.js
+++ features/vaccineData.js
@@ -1,17 +1,15 @@
-const vaccine = require("../repositories/vaccineOps.js")
+const vaccine = require("../repositories/vaccineOps.js");
 
 //Should Take in vaccine keyword and two letter state
 module.exports = function (app) {
-
-   //Regular Expression search (vaccine + State Abbreviation)
-   let vaccineSearch =  /vaccine (AL|AK|AZ|AR|CA|CO|CT|DE|FL|GA|HI|ID|IL|IN|IA|KS|KY|LA|ME|D|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VT|VA|WA|WV|WI|WY)/;
+  //Regular Expression search (vaccine + State Abbreviation)
+  let vaccineSearch =
+    /vaccine (AL|AK|AZ|AR|CA|CO|CT|DE|FL|GA|HI|ID|IL|IN|IA|KS|KY|LA|ME|D|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VT|VA|WA|WV|WI|WY)/;
   app.message(vaccineSearch, vaccineData);
-
 };
 
 async function vaccineData({ message, client }) {
-
-  //Cut out State Abbriviation into array 
+  //Cut out State Abbriviation into array
   stateAbbriv = message.text;
   const stateAbv = message.text.split(" ", 2);
 
@@ -21,6 +19,11 @@ async function vaccineData({ message, client }) {
   await client.chat.postEphemeral({
     channel: message.channel,
     user: message.user,
-    text: ("Percentage Vaccinated in " + stateAbv[1] + ": " + vaccinedata[0].series_complete_pop_pct + "%"),
+    text:
+      "Percentage Vaccinated in " +
+      stateAbv[1] +
+      ": " +
+      vaccinedata[0].series_complete_pop_pct +
+      "%",
   });
 }
--- repositories/vaccineOps.js
+++ repositories/vaccineOps.js
@@ -1,14 +1,20 @@
-const axios = require('axios');
+const axios = require("axios");
 
 module.exports = {
-    getUsData: async (state) => {
+  getUsData: async (state) => {
+    //Get current data for most recent date
+    var yesterday = new Date(new Date().valueOf() - 1000 * 60 * 60 * 24)
+      .toISOString()
+      .slice(0, 10);
+    let apicall =
+      "https://data.cdc.gov/resource/unsk-b7fc.json?location=" +
+      state +
+      "&date=" +
+      yesterday +
+      "T00:00:00.000";
 
-        //Get current data for most recent date
-        var yesterday = new Date((new Date()).valueOf() - 1000*60*60*24).toISOString().slice(0, 10);
-        let apicall = 'https://data.cdc.gov/resource/unsk-b7fc.json?location=' + state + '&date=' + yesterday + 'T00:00:00.000';
-        
-        //Use axios library for https request	
-	const response = await axios(apicall);
-	return response.data;
-    	}
-}
+    //Use axios library for https request
+    const response = await axios(apicall);
+    return response.data;
+  },
+};
