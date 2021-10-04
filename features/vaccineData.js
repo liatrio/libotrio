6ff73fd21d6e074cd7cb2761447457bfcc2bb14e
Micah Perez II <micahperez@Micahs-MacBook-Pro.local>
2021-10-04 10:13:18 -0700

    feat: add vaccine module and feature

---
+++ features/vaccineData.js
@@ -0,0 +1,26 @@
+const vaccine = require("../repositories/vaccineOps.js")
+
+//Should Take in vaccine keyword and two letter state
+module.exports = function (app) {
+
+   //Regular Expression search (vaccine + State Abbreviation)
+   let vaccineSearch =  /vaccine (AL|AK|AZ|AR|CA|CO|CT|DE|FL|GA|HI|ID|IL|IN|IA|KS|KY|LA|ME|D|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VT|VA|WA|WV|WI|WY)/;
+  app.message(vaccineSearch, vaccineData);
+
+};
+
+async function vaccineData({ message, client }) {
+
+  //Cut out State Abbriviation into array 
+  stateAbbriv = message.text;
+  const stateAbv = message.text.split(" ", 2);
+
+  //Use input state to retrieve data
+  const vaccinedata = await vaccine.getUsData(stateAbv[1]);
+
+  await client.chat.postEphemeral({
+    channel: message.channel,
+    user: message.user,
+    text: ("Percentage Vaccinated in " + stateAbv[1] + ": " + vaccinedata[0].series_complete_pop_pct + "%"),
+  });
+}
---
+++ repositories/vaccineOps.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+module.exports = {
+    getUsData: async (state) => {
+
+        //Get current data for most recent date
+        var yesterday = new Date((new Date()).valueOf() - 1000*60*60*24).toISOString().slice(0, 10);
+        let apicall = 'https://data.cdc.gov/resource/unsk-b7fc.json?location=' + state + '&date=' + yesterday + 'T00:00:00.000';
+        
+        //Use axios library for https request	
+	const response = await axios(apicall);
+	return response.data;
+    	}
+}
