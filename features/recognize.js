dff52483ec8190cb2eaeb48118099c2391576b27
bserrato7900 <benjamins@liatrio.com>
2022-09-15 10:18:13 -0700

    fix: changed promise return calls to await due to deprecation of the promise call.

--- features/recognize.js
+++ features/recognize.js
@@ -1,4 +1,5 @@
 const emoji = require("../services/recognizeServ");
+const { ReactionMatches } = require("../middleware");
 
 module.exports = function (app) {
   app.message(
@@ -6,41 +7,28 @@ module.exports = function (app) {
     //   anyOf(directMention(), directMessage()),
     recognize
   );
-  app.event("reaction_added", reactionMatches(":beerjar:"), reaction);
+  app.event("reaction_added", ReactionMatches(":beerjar:"), Reaction);
 };
 
 async function recognize({ message, client }) {
   const response = emoji.respond();
 
-  return Promise.all([
-    client.chat.postEphemeral({
-      channel: message.channel,
-      user: message.user,
-      text: response,
-    }),
-    client.reactions.add({
-      channel: message.channel,
-      name: "beerjar",
-      timestamp: message.ts,
-    }),
-  ]);
+  await client.chat.postEphemeral({
+    channel: message.channel,
+    user: message.user,
+    text: response,
+  });
+  await client.reactions.add({
+    channel: message.channel,
+    name: "beerjar",
+    timestamp: message.ts,
+  });
 }
 
-async function reaction({ event, client }) {
+async function Reaction({ event, client }) {
   await client.chat.postEphemeral({
     channel: event.item.channel,
     user: event.user,
     text: `You reacted to a message with beerjar`,
   });
-}
-
-function reactionMatches(emoji) {
-  return async ({ event, next }) => {
-    if (emoji[0] == ":" && emoji[emoji.length - 1] == ":") {
-      emoji = emoji.slice(1, -1);
-    }
-    if (event.reaction.includes(emoji)) {
-      await next();
-    }
-  };
 }
