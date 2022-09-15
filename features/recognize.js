616f9e94905b2bc6df3b6b603a11aab4ffcc9ca9
bserrato7900 <benjamins@liatrio.com>
2022-09-15 08:27:23 -0700

    feat: emoji reaction to the recognized message and give feedback to other user reactions to beerjar

--- features/recognize.js
+++ features/recognize.js
@@ -6,14 +6,41 @@ module.exports = function (app) {
     //   anyOf(directMention(), directMessage()),
     recognize
   );
+  app.event("reaction_added", reactionMatches(":beerjar:"), reaction);
 };
 
 async function recognize({ message, client }) {
   const response = emoji.respond();
 
+  return Promise.all([
+    client.chat.postEphemeral({
+      channel: message.channel,
+      user: message.user,
+      text: response,
+    }),
+    client.reactions.add({
+      channel: message.channel,
+      name: "beerjar",
+      timestamp: message.ts,
+    }),
+  ]);
+}
+
+async function reaction({ event, client }) {
   await client.chat.postEphemeral({
-    channel: message.channel,
-    user: message.user,
-    text: response,
+    channel: event.item.channel,
+    user: event.user,
+    text: `You reacted to a message with beerjar`,
   });
+}
+
+function reactionMatches(emoji) {
+  return async ({ event, next }) => {
+    if (emoji[0] == ":" && emoji[emoji.length - 1] == ":") {
+      emoji = emoji.slice(1, -1);
+    }
+    if (event.reaction.includes(emoji)) {
+      await next();
+    }
+  };
 }
