15ce118635c1b5b3c5ab6f5130cec418c4bba7f8
Evan Drake <evanmdrake@icloud.com>
2022-09-19 15:36:09 -0700

    fixed linting errors, added description block to file. with todo listing current state

--- features/tabCount.js
+++ features/tabCount.js
@@ -1,18 +1,23 @@
-'use strict';
-const MySql = require('mysql2');
-
-const { resolveConfig } = require("prettier");
+"use strict";
+/*
+ * This file adds functionality for users to query their respective
+ * 'tabs' or the amount of beer jars they currently possess at the
+ * time. it does this by querying a db and returning all unresolved
+ * beer jars connected to the user.
+ *
+ * in its current form:
+ * TODO
+ * - not tested
+ *   no db connection
+ */
 
 module.exports = function (app) {
-  app.message(
-    ":cheers_to_the_beer_jar: tab",
-    SendTab
-  );
+  app.message(":cheers_to_the_beer_jar: tab", SendTab);
 };
 
-async function SendTab({message, client}) {
+async function SendTab({ message, client }) {
   await client.chat.postMessage({
     channel: message.user,
-    text: ("\`" + "1" + "\`")
+    text: "`" + "1" + "`",
   });
-};
+}
