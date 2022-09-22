51d3396ec32a3790c15d1f39dca8595f49a83f6d
Evan Drake <evanmdrake@icloud.com>
2022-09-22 09:17:51 -0700

    made test for poolcreation

--- test/services/beepBoopTest.js
+++ test/services/beepBoopTest.js
@@ -1,5 +1,8 @@
 const expect = require("chai").expect;
+const sinon = require("sinon");
 const beepBoop = require("../../services/beepBoopServ").respond;
+const dbops = require("../../repositories/databaseops");
+const MySql = require("mysql2/promise");
 
 describe("service/beepBoop", () => {
   describe("beepBoopServ", () => {
@@ -9,4 +12,16 @@ describe("service/beepBoop", () => {
       expect(beepBoop()).to.equal(`BOOP #3 since last restart`);
     });
   });
+  describe("databaseOps", () => {
+    beforeEach(
+      ()=>{
+        sinon.restore()
+        poolMock = sinon.stub(MySql, "createPool")
+    });
+    it("mocking db connection", () => {
+      dbops.setupPool();
+      expect(poolMock).to.have.been.calledOnce;
+    });
+  });
+  
 });
