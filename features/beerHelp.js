334baa566180a78d679020e59b62d886c3b6b3e9
Steven Mendez <96746466+MinecraftSt3v3@users.noreply.github.com>
2022-09-23 13:59:17 -0700

    Help message for BeerJar app

--- features/beerHelp.js
+++ features/beerHelp.js
@@ -3,7 +3,7 @@
 // obvious to users
 
 module.exports = function (app) {
-  app.message("beerJar help", helpMe);
+  app.message("beerjar help", helpMe);
 };
 
 const helpMD = `
@@ -14,17 +14,22 @@ How to use
 > @benny For missing this mornings standup :cheers_to_the_beer_jar: 
  Note: people can get stacked for beers by adding the beer emoji as a reaction.
 
-After a certain threshhold of 25 beerjars you will have to buy a beer for the person who beer jared you 
+You can also tag multiple people.
+> @benny @ryan @evan @zack @steven for forgetting to lock up the office :cheers_to_beer_jar:
+
+*Not implemented yet*
+After a certain threshold of 25 beerjars you will have to buy a beer for the person who beer jared you 
 
 A multiplier can be added to give your daily beerjars away.
 >@benny for missing standup :cheers_to_the_beer_jar:x5 
 
+*Not implementedyet*
+Viewing your balance
+> beerjar check balance
 
-viewing your balance
-> @beerjar check balance
-
+*Not implemented yet*
 Viewing the scoreboard
-> @beerjar show scores
+> beerjar show score
 
 `;
 
